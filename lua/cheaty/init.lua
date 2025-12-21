d24f6498268cf1040d1af3c13fe7e86a21a059d5
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2025-12-21 18:54:55 +0200

    feat: init.lua

---
+++ lua/cheaty/init.lua
@@ -0,0 +1,20 @@
+local M = {}
+
+local defaults = {
+	keymap     = "<leader>?",
+	width      = 0.6,
+	height     = 0.6,
+	cheatsheet = { "This is a placeholder cheatsheet!", "Add your own in the config!" }
+}
+
+M.config = {}
+
+function M.setup(opts)
+	M.config = vim.tbl_deep_extend("force", defaults, opts or {})
+
+	vim.keymap.set('n', M.config.keymap, function()
+		require("cheaty.window").toggle(M.config)
+	end, { desc = "Toggle cheaty.nvim cheatsheet" })
+end
+
+return M
