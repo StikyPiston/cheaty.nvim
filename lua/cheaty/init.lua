84b153c55c4a8dddc07eb26ba0922b4b9be6557e
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2026-01-04 20:21:01 +0200

    feat!: Cheaty user command

--- lua/cheaty/init.lua
+++ lua/cheaty/init.lua
@@ -1,7 +1,6 @@
 local M = {}
 
 local defaults = {
-	keymap     = "<leader>cs",
 	width      = 0.6,
 	height     = 0.6,
 	cheatsheet = { "# This is a sample cheatsheet!", "Tailor it to your liking in the config!" }
@@ -12,9 +11,11 @@ M.config = {}
 function M.setup(opts)
 	M.config = vim.tbl_deep_extend("force", defaults, opts or {})
 
-	vim.keymap.set('n', M.config.keymap, function()
-		require("cheaty.window").toggle(M.config)
-	end, { desc = "Toggle cheaty.nvim cheatsheet" })
+	vim.api.nvim_create_user_command(
+		'Cheaty',
+		function() require("cheaty.window").toggle(M.config) end,
+		{ desc = "Open Cheatsheet" }
+	)
 end
 
 return M
